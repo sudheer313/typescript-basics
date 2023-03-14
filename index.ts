f41243a5461f89d44540dffbdc96074ed2d34019
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-15 09:29:36 +1100

    datatypes

--- index.ts
+++ index.ts
@@ -4,3 +4,35 @@ function greeter(name:string) {
     }
 
 greeter("World")
+
+//number
+
+let count:number=5;
+
+// string
+
+let name1:string="Nihar";
+
+//Boolean
+
+let isTrue:boolean=true;
+
+//Array
+
+let list:number[]=[1,2,3,4];
+
+//Tuple
+let person:[string,number]=["John",30];
+
+//enum
+enum Color{Red,Green,Blue}
+let color:Color=Color.Red;
+//Any
+let anything:any="hello";
+anything=5;
+
+//Null and undefined
+
+let n:null=null;
+let u:undefined=undefined;
+
