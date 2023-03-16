9aa0b7da752b61affed352f632ef5ed8922d7799
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-16 21:49:42 +1100

    updated typescripe basics  Interface

--- index.js
+++ index.js
@@ -1,4 +1,38 @@
-function greeter(name) {
-    console.log("Hello, ".concat(name, "!"));
-}
-greeter("World");
+// function greeter(name:string) {
+//     console.log(`Hello, ${name}!`);
+//     }
+// greeter("World")
+// //number
+// let count:number=5;
+// // string
+// let name1:string="Nihar";
+// //Boolean
+// let isTrue:boolean=true;
+// //Array
+// let list:number[]=[1,2,3,4];
+// //Tuple
+// let person:[string,number]=["John",30];
+// //enum
+// enum Color{Red,Green,Blue}
+// let color:Color=Color.Red;
+// //Any
+// let anything:any="hello";
+// anything=5;
+// //Null and undefined
+// let n:null=null;
+// let u:undefined=undefined;
+//print a string
+var myName = "Nihar";
+console.log(myName);
+//add two numbers
+var sum = function add(a, b) {
+    return a + b;
+};
+console.log(sum(2, 3));
+var john = {
+    name: "John",
+    age: 30
+};
+console.log(john);
+console.log(john.name);
+console.log(john.age);
--- index.ts
+++ index.ts
@@ -1,38 +1,65 @@
-function greeter(name:string) {
-    console.log(`Hello, ${name}!`);
+// function greeter(name:string) {
+//     console.log(`Hello, ${name}!`);
     
-    }
+//     }
 
-greeter("World")
+// greeter("World")
 
-//number
+// //number
 
-let count:number=5;
+// let count:number=5;
 
-// string
+// // string
 
-let name1:string="Nihar";
+// let name1:string="Nihar";
 
-//Boolean
+// //Boolean
 
-let isTrue:boolean=true;
+// let isTrue:boolean=true;
 
-//Array
+// //Array
 
-let list:number[]=[1,2,3,4];
+// let list:number[]=[1,2,3,4];
 
-//Tuple
-let person:[string,number]=["John",30];
+// //Tuple
+// let person:[string,number]=["John",30];
 
-//enum
-enum Color{Red,Green,Blue}
-let color:Color=Color.Red;
-//Any
-let anything:any="hello";
-anything=5;
+// //enum
+// enum Color{Red,Green,Blue}
+// let color:Color=Color.Red;
+// //Any
+// let anything:any="hello";
+// anything=5;
 
-//Null and undefined
+// //Null and undefined
 
-let n:null=null;
-let u:undefined=undefined;
+// let n:null=null;
+// let u:undefined=undefined;
 
+//print a string
+let myName:string="Nihar";
+
+console.log(myName);
+
+//add two numbers
+const sum=function add(a:number,b:number):number {
+    return a+b;
+    
+}
+console.log(sum(2,3));
+
+//interface
+
+interface Person{
+    name:string;
+    age:number;
+}
+
+let john:Person={
+    name:"John",
+    age:30
+}
+
+console.log(john);
+console.log(john.name);
+console.log(john.age);
