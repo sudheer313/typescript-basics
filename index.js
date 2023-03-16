a756b7f4a9f946033765e1bdb8faf95c38ade8d1
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-03-16 22:35:46 +1100

    update basics with employee obj

--- index.js
+++ index.js
@@ -1,38 +1,26 @@
 // function greeter(name:string) {
 //     console.log(`Hello, ${name}!`);
-//     }
-// greeter("World")
-// //number
-// let count:number=5;
-// // string
-// let name1:string="Nihar";
-// //Boolean
-// let isTrue:boolean=true;
-// //Array
-// let list:number[]=[1,2,3,4];
-// //Tuple
-// let person:[string,number]=["John",30];
-// //enum
-// enum Color{Red,Green,Blue}
-// let color:Color=Color.Red;
-// //Any
-// let anything:any="hello";
-// anything=5;
-// //Null and undefined
-// let n:null=null;
-// let u:undefined=undefined;
-//print a string
-var myName = "Nihar";
-console.log(myName);
-//add two numbers
-var sum = function add(a, b) {
-    return a + b;
+var nihar = {
+    id: 123,
+    firstName: "Nihar",
+    lastName: "Kandula",
+    email: "nihar@gmail.com",
+    departments: ["Sales", "Marketing"],
+    hireDate: new Date(),
+    isManager: false,
+    getFullName: function () {
+        return "".concat(this.firstName, " ").concat(this.lastName);
+    },
+    getDepartmentList: function (showIds) {
+        if (showIds === void 0) { showIds = true; }
+        if (showIds) {
+            return this.departments.map(function (dept, i) { return "".concat(i + 1, ". ").concat(dept); });
+        }
+        else {
+            return this.departments;
+        }
+    }
 };
-console.log(sum(2, 3));
-var john = {
-    name: "John",
-    age: 30
-};
-console.log(john);
-console.log(john.name);
-console.log(john.age);
+console.log(nihar.getFullName());
+console.log(nihar.getDepartmentList());
+console.log(nihar.hireDate);
--- index.ts
+++ index.ts
@@ -36,30 +36,76 @@
 // let n:null=null;
 // let u:undefined=undefined;
 
-//print a string
-let myName:string="Nihar";
+// //print a string
+// let myName:string="Nihar";
 
-console.log(myName);
+// console.log(myName);
 
-//add two numbers
-const sum=function add(a:number,b:number):number {
-    return a+b;
+// //add two numbers
+// const sum=function add(a:number,b:number):number {
+//     return a+b;
     
-}
-console.log(sum(2,3));
+// }
+// console.log(sum(2,3));
 
-//interface
+// //interface
 
-interface Person{
-    name:string;
-    age:number;
-}
+// interface Person{
+//     name:string;
+//     age:number;
+// }
 
-let john:Person={
-    name:"John",
-    age:30
-}
+// let john:Person={
+//     name:"John",
+//     age:30
+// }
 
-console.log(john);
-console.log(john.name);
-console.log(john.age);
+// console.log(john);
+// console.log(john.name);
+// console.log(john.age);
+
+//Interface Employee
+
+interface Employee {
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    phone?: string;
+    address?: {
+      street: string;
+      city: string;
+      state: string;
+      zip: string;
+    };
+    departments: string[];
+    hireDate: Date;
+    isManager: boolean;
+    getFullName: () => string;
+    getDepartmentList: (showIds?: boolean) => string[];
+  }
+  
+  let nihar: Employee = {
+    id: 123,
+    firstName: "Nihar",
+    lastName: "Kandula",
+    email: "nihar@gmail.com",
+    departments: ["Sales", "Marketing"],
+    hireDate: new Date(),
+    isManager: false,
+    getFullName: function() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    getDepartmentList: function(showIds = true) {
+      if (showIds) {
+        return this.departments.map((dept, i) => `${i + 1}. ${dept}`);
+      } else {
+        return this.departments;
+      }
+    }
+  };
+  
+  console.log(nihar.getFullName());
+  console.log(nihar.getDepartmentList());
+  console.log(nihar.hireDate);
+  
