32d3c99fa20aca217781304e714ff6d372fa9c10
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-04-20 23:57:43 +1000

    update and modify package.json

--- dist/index.js
+++ dist/index.js
@@ -1,71 +1,48 @@
+"use strict";
 // function greeter(name:string) {
 //     console.log(`Hello, ${name}!`);
-    
 //     }
-
 // greeter("World")
-
 // //number
-
 // let count:number=5;
-
 // // string
-
 // let name1:string="Nihar";
-
 // //Boolean
-
 // let isTrue:boolean=true;
-
 // //Array
-
 // let list:number[]=[1,2,3,4];
-
 // //Tuple
 // let person:[string,number]=["John",30];
-
 // //enum
 // enum Color{Red,Green,Blue}
 // let color:Color=Color.Red;
 // //Any
 // let anything:any="hello";
 // anything=5;
-
 // //Null and undefined
-
 // let n:null=null;
 // let u:undefined=undefined;
-
 // //print a string
 // let myName:string="Nihar";
-
 // console.log(myName);
-
 // //add two numbers
 // const sum=function add(a:number,b:number):number {
 //     return a+b;
-    
 // }
 // console.log(sum(2,3));
-
 // //interface
-
 // interface Person{
 //     name:string;
 //     age:number;
 // }
-
 // let john:Person={
 //     name:"John",
 //     age:30
 // }
-
 // console.log(john);
 // console.log(john.name);
 // console.log(john.age);
-
 //Interface Employee
-
 // interface Employee {
 //     id: number;
 //     firstName: string;
@@ -84,7 +61,6 @@
 //     getFullName: () => string;
 //     getDepartmentList: (showIds?: boolean) => string[];
 //   }
-  
 //   let nihar: Employee = {
 //     id: 123,
 //     firstName: "Nihar",
@@ -104,17 +80,8 @@
 //       }
 //     }
 //   };
-  
 //   console.log(nihar.getFullName());
 //   console.log(nihar.getDepartmentList());
 //   console.log(nihar.hireDate);
-  
-interface Person {
-  firstName: string;
-  lastName: string;
-}
-
-function greet(person: Person): string {
-  return `Hello, ${person.firstName} ${person.lastName}`;
-}
-
+let id = 5;
+id = '5';
--- index.js
+++ index.js
@@ -1,26 +1,6 @@
+"use strict";
 // function greeter(name:string) {
 //     console.log(`Hello, ${name}!`);
-var nihar = {
-    id: 123,
-    firstName: "Nihar",
-    lastName: "Kandula",
-    email: "nihar@gmail.com",
-    departments: ["Sales", "Marketing"],
-    hireDate: new Date(),
-    isManager: false,
-    getFullName: function () {
-        return "".concat(this.firstName, " ").concat(this.lastName);
-    },
-    getDepartmentList: function (showIds) {
-        if (showIds === void 0) { showIds = true; }
-        if (showIds) {
-            return this.departments.map(function (dept, i) { return "".concat(i + 1, ". ").concat(dept); });
-        }
-        else {
-            return this.departments;
-        }
-    }
-};
-console.log(nihar.getFullName());
-console.log(nihar.getDepartmentList());
-console.log(nihar.hireDate);
+function greet(person) {
+    return "Hello, ".concat(person.firstName, " ").concat(person.lastName);
+}
