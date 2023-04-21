59250b675f17b1efaf34fdba31904635a7a083dc
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-04-21 21:11:56 +1000

    updated Html and Css

--- index.ts
+++ index.ts
@@ -109,12 +109,5 @@
 //   console.log(nihar.getDepartmentList());
 //   console.log(nihar.hireDate);
   
-interface Person {
-  firstName: string;
-  lastName: string;
-}
-
-function greet(person: Person): string {
-  return `Hello, ${person.firstName} ${person.lastName}`;
-}
+console.log("Sudheer");
 
